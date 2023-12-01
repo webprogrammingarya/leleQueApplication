1f23db087b032aea45bbe3911fb9f21be9889872
Web Programming Arya <133558328+webprogrammingarya@users.noreply.github.com>
2023-12-01 23:37:47 +0700

    Delete application directory

--- application/views/templates/auth_footer.php
+++
@@ -1,5 +0,0 @@
-<script src="<?= base_url('assets/'); ?>/libs/jquery/dist/jquery.min.js"></script>
-<script src="<?= base_url('assets/'); ?>/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-
-</html>
